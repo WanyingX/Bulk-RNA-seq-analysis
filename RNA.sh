8e3b2d7019837ef1a19c10a7831272ee3d7ff1f8
Wanying Xu <52838412+WanyingX@users.noreply.github.com>
2021-09-03 17:13:30 -0400

    Update and rename mapping_RNA.sh to RNA.sh

--- RNA.sh
+++ RNA.sh
@@ -14,23 +14,23 @@ fq=$2
 
 name=$3
 
-# Here is index of reference genome which is used to do mapping
+#----------------Build your mapping index----------------------
 
 index=$ref/${genome}
 
-# Step2 : Start mapping your fastq file (raw read)
+#----------------Step1: Start mapping--------------------------
 
 echo Total read `cat ${fq}_1.fastq | grep ^@ | wc -l` >> summary
 
 hisat2 -x $index -p 7 --sp 1000,1000 -1 ${fq}_1.fastq -2 ${fq}_2.fastq  | samtools view -bS - > $name.bam
 
-# Step3: Keep uniquely mapped reads and then sort them by using coordinate before we remove PCR duplication.
+#------------Step2: Keep uniquely mapped reads-----------------
 
 samtools view -F 1804 -b $name.bam | samtools sort -@ 12 - > $name.sorted.bam
 
 echo Uniquely mapped reads `samtools view -c $name.sorted.bam` >> summary
 
-# Step4: Remove PCR duplication by using picard
+#------------Step3: remove PCR duplication---------------------
 
 java -jar -Xmx4g $software/picard-tools/MarkDuplicates.jar \
       I=$name.sorted.bam \
@@ -38,7 +38,7 @@ java -jar -Xmx4g $software/picard-tools/MarkDuplicates.jar \
       M=marked_dup_metrics.txt
 
 echo Non-redundant mapped reads `samtools view -c ` >> summary
-# Step5: Annotate your non-duplicated reads into transcription by featureCounts.
+#-------Step4: Annotate genes by using reads--------------------
 
 gtf=$ref/${genome}.refFlat.gtf
 
@@ -46,6 +46,17 @@ featureCounts --ignoreDup -p -B -a $gtf -t exon -g gene_id -o counts.txt ${name}
 
 cut -f1,7 counts.txt > $name.count.bed
 rm $name.bam $name.sorted.bam
+
+#!/bin/bash
+#---------Generate genomic track of your RNA-seq data------------
+
+chrom_size=$ref/$genome.chrom.sizes
+$software/genomeCoverageBed -bg -ibam ${name}.dedup.bam -g $chrom_size > $name.bedgraph
+
+sort -k1,1 -k2,2n $name.bedgraph -o $name.bedgraph
+
+$software/bedGraphToBigWig $name.bedgraph $chrom_size $name.bw
+chmod 644 *.bw
 
 wait
 exit;

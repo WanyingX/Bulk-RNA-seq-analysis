f60594075b523060b4e575372958cc52d8360805
Wanying Xu <52838412+WanyingX@users.noreply.github.com>
2021-09-03 16:55:54 -0400

    Update RNA-seq_track.sh

--- RNA-seq_track.sh
+++ RNA-seq_track.sh
@@ -1,44 +1,22 @@
 #!/bin/bash
-#fq=$1
-name=$1
 
-#index=/mnt/rds/genetics01/JinLab/xww/Reference_Index/mm10/mm10
+genome=$1
+name=$2
 
-#hisat2 -x $index -p 7 --sp 1000,1000 -1 ${fq}_1.fastq -2 ${fq}_2.fastq  | samtools view -bS - > $name.merge.bam
+if [ $genome == "mm10" ];then
+  chrom_size=/mnt/rstor/genetics/JinLab/xxl244/Reference_Indexes/mm10_bowtie_index/mm10.chrom.sizes
+fi
 
-#wait
+if [ $genome == "hg19" ];then
+  chrom_size=/mnt/rstor/genetics/JinLab/xxl244/Reference_Indexes/hg19/Homo_sapiens/UCSC/hg19/Sequence/Chromosomes/chrom.sizes
+fi
 
-#gtf=/mnt/rds/genetics01/JinLab/xww/RNA-seq/hg19.refFlat.gtf
-
-#samtools sort $name.merge.bam > $name.merge.sorted.bam &
-samtools merge $name.bam $name.rep1.merge.sorted.bam $name.rep2.merge.sorted.bam $name.rep3.merge.sorted.bam 
-#wait
-
-samtools sort $name.bam > $name.sorted.bam
-
-#wait
-
-
-#genomeCoverageBed -bg -ibam $name.merge.sorted.bam -g $chrom_size -strand - | awk '{$4 = - $4;print $0}' > $name.rev.bedgraph
-
-# mouse chrom size 
-chrom_size=/mnt/rstor/genetics/JinLab/xxl244/Reference_Indexes/mm10_bowtie_index/mm10.chrom.sizes
-
-# human chrom size
-chrom_size=/mnt/rstor/genetics/JinLab/xxl244/Reference_Indexes/hg19/Homo_sapiens/UCSC/hg19/Sequence/Chromosomes/chrom.sizes
-
-genomeCoverageBed -bg -ibam $name.sorted.bam -g $chrom_size > $name.bedgraph
-
-wait
+$software/genomeCoverageBed -bg -ibam $name.dedup.bam -g $chrom_size > $name.bedgraph
 
 sort -k1,1 -k2,2n $name.bedgraph -o $name.bedgraph
 
-bedGraphToBigWig $name.bedgraph $chrom_size $name.bw
-#bedGraphToBigWig $name.forward.bedgraph $chrom_size $name.forward.bw &
-#bedGraphToBigWig $name.reverse.bedgraph $chrom_size $name.reverse.bw &
-
-wait
+$software/bedGraphToBigWig $name.bedgraph $chrom_size $name.bw
 chmod 644 *.bw
-wait
 
+wait
 exit

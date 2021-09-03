69fc3137486246efa39bf75a309fd83724fa01c4
Wanying Xu <52838412+WanyingX@users.noreply.github.com>
2021-09-03 16:59:02 -0400

    Update RNA-seq_track.sh

--- RNA-seq_track.sh
+++ RNA-seq_track.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-
+#----------Build your own path--------------
+software=
 genome=$1
 name=$2
 
-if [ $genome == "mm10" ];then
-  chrom_size=/mnt/rstor/genetics/JinLab/xxl244/Reference_Indexes/mm10_bowtie_index/mm10.chrom.sizes
-fi
 
-if [ $genome == "hg19" ];then
-  chrom_size=/mnt/rstor/genetics/JinLab/xxl244/Reference_Indexes/hg19/Homo_sapiens/UCSC/hg19/Sequence/Chromosomes/chrom.sizes
-fi
-
+chrom_size=$ref/$genome.chrom.sizes
 $software/genomeCoverageBed -bg -ibam $name.dedup.bam -g $chrom_size > $name.bedgraph
 
 sort -k1,1 -k2,2n $name.bedgraph -o $name.bedgraph
